e5bcac33dacd7693a68703a8af2d5efc27e8eea5
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-05-27 20:48:20 +0530

    Git Auto Push

---
+++ app-deploy.tf
@@ -0,0 +1,16 @@
+# #Installing the Application
+# resource "null_resource" "app" {
+#   count = local.INSTANCE_COUNT
+
+#   connection {
+#     type     = "ssh"
+#     user     = local.SSH_USERNAME
+#     password = local.SSH_PASSWORD
+#     host     = element(local.INSTANCE_PRIVATE_IPS, count.index)
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#     ]
+#   }
+
+# }
