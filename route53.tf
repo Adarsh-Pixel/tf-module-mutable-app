071ebc948a17478ab8f272e3bed0823e234b865d
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-01 15:42:28 +0530

    Git Auto Push

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 # Creates app component records against the ALB
 resource "aws_route53_record" "docdb_dns" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  zone_id = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
   name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [aws_docdb_cluster.docdb.endpoint]
+  records = []
 }
