c0796ff1e447576126fbcdb957683bf14083abb7
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-01 13:38:09 +0530

    Git Auto Push

--- app-deploy.tf
+++ app-deploy.tf
@@ -2,13 +2,16 @@
 resource "null_resource" "app" {
   count = local.INSTANCE_COUNT
 
-  connection {
-    type     = "ssh"
-    user     = local.SSH_USERNAME
-    password = local.SSH_PASSWORD
-    host     = element(local.INSTANCE_PRIVATE_IPS, count.index)
+  triggers = {
+    always_run = "${timestamp()}"
   }
+
   provisioner "remote-exec" {
+      connection {
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
+      host     = element(local.INSTANCE_PRIVATE_IPS, count.index)
+    }
     inline = [
         "ansible-pull -U https://github.com/Adarsh-Pixel/ansible.git -e ENG=dev -e COMPONENT=${var.COMPONENT} roboshop-pull.yml"
     ]
