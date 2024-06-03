6ee810625bf1de690eb7bc01f949ca53473ad2d5
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-03 16:23:24 +0530

    Git Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -4,6 +4,16 @@ resource "aws_lb_target_group" "app" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    timeout             = 4
+    path                = "/health"
+    unhealthy_threshold = 2
+  }
 }
 
 # Attach the Target Group to load balancer
