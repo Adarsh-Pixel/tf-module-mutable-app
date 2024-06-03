9b24337af0fa23994c165e26af28bcbc5e10316c
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-03 14:34:15 +0530

    Git Auto Push

--- listerner.tf
+++ listerner.tf
@@ -9,7 +9,12 @@ resource "aws_lb_listener" "private" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
   }
+}
 
+#Generates a random number in the given range
+resource "random_integer" "priority" {
+  min = 100
+  max = 500
 }
 
   # Creates a rule in the PRIVATE load balancer listener 
@@ -18,7 +23,7 @@ resource "aws_lb_listener_rule" "app_rule" {
     count      = var.INTERNAL ? 1 : 0 
 
     listener_arn = aws_lb_listener.private.*.arn[0]
-    priority     = 99
+    priority     = random_integer.priority.result
 
     action {
         type             = "forward"
