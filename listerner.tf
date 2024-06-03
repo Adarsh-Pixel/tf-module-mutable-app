794ae0895418064f6a6c084eded0b502457cc72b
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-03 15:08:04 +0530

    Git Auto Push

--- listerner.tf
+++ listerner.tf
@@ -1,16 +1,3 @@
-# This creates the listernr and adds to the PRIVATE ALB
-resource "aws_lb_listener" "private" {
-  count             = var.INTERNAL ? 1 : 0 
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
-
 #Generates a random number in the given range
 resource "random_integer" "priority" {
   min = 100
