bfb552a943bb6eb17a8979d4b79c44e83d205462
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-07-12 20:01:14 +0530

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,6 @@
 resource "aws_spot_instance_request" "spot" {
   count                 = var.SPOT_INSTANCE_COUNT
   ami                   = data.aws_ami.ami.image_id
-  spot_price            = "0.03"
   instance_type         = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment  = true
   vpc_security_group_ids  = [aws_security_group.allows_app.id]
